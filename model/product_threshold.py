f5c3366dfd93bcb7d5b5ad69fd1912df507b6476
MariPinas <marianapls2003@gmail.com>
2025-06-09 13:21:00 -0300

    :sparkles: add methods

--- model/product_threshold.py
+++ model/product_threshold.py
@@ -8,3 +8,10 @@ class ProductThreshold:
         self.pest = pest
         self.minimumOccurences = minimumOccurences
         self.dosePerHectare = dosePerHectare
+    
+    def is_applicable(self, occurrences: int) -> bool:
+        return occurrences >= self.minimumOccurences
+
+    def calculate_total_dose(self, area: float) -> float:
+        return self.dosePerHectare * area
+    
